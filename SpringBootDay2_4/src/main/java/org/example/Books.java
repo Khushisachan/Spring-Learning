b7a88390caa024d2fb002b059b18879dd0ea2cce
Khushi sachan <Khushisachan35@gmail.com>
2026-02-10 19:24:09 +0530

    assignment files

---
+++ SpringBootDay2_4/src/main/java/org/example/Books.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Books {
+
+    private String bookName;
+    private float price;
+    private String authorName;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}

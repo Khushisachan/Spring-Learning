1d7d7692605df46854c5c9a65aaa22e7c196575c
Khushi sachan <Khushisachan35@gmail.com>
2026-02-10 11:06:28 +0530

    springBoot Files

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,23 @@
+package org.example;
+
+// pojo class
+public class Person {
+    public void  person(){
+        System.out.println("object is created");
+    }
+    public void m1(){
+        System.out.println("hello spring");
+    }
+    public void m2(){
+        System.out.println("init method");
+    }
+    public void m3(){
+        System.out.println("destroy method");
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
